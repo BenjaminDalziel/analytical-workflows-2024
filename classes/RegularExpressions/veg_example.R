4198b551f23860a0da40d1d4fddaff0ba9c58b3b
Benjamin Dalziel <benjamin.dalziel@oregonstate.edu>
2024-03-04 11:00:59 -0800

    Update veg example

--- classes/RegularExpressions/veg_example.R
+++ classes/RegularExpressions/veg_example.R
@@ -6,15 +6,25 @@ graphics.off()
 
 # Sample data
 dat <- data.frame(
-    site_id = c(1, 2, 3, 4, 5),
-    veg_type = c("Forest", "Woodland", "Rain Forest", "Mangoev", "grassland")
+    site_id = c(1, 2, 3, 4, 5, 6),
+    veg_type = c("Forest",
+                "Woodland",
+                "Rain Forest",
+                "Mangoev",
+                "grassland",
+                "rainforest")
 )
 
 
-# Define a correction strategy
+## Define a correction strategy
 correction_strategy <- list(
     "Mangoev" = "Mangrove",
-    "Rain Forest" = "Rainforest"
+    "Rain Forest" = "Rainforest",
+    #"grassland" = "Grassland",     #instead of this...
+    #"rainforest" = "Rainforest",   #...and this...
+    "^(\\w)" = "\\U\\1"             #...use a (Perl-style) regular expression
+                                    #that will capitalize the first letter of
+                                    #each word that needs it
 )
 
 # Use gsub function to correct the data
@@ -22,5 +32,5 @@ nerrortype <- length(correction_strategy)
 for (i in 1:nerrortype) {
     pattern <- names(correction_strategy)[i]
     repl <- correction_strategy[[i]]
-    dat$veg_type <- gsub(pattern, repl, dat$veg_type)
+    dat$veg_type <- gsub(pattern, repl, dat$veg_type, perl = TRUE)
 }
